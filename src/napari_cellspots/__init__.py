c76c1132918291af6c3ebaffbdf5ace48c8e2406
Guillaume Witz <guillaume.witz@unibe.ch>
2026-03-30 15:12:38 +0200

    automate version

--- src/napari_cellspots/__init__.py
+++ src/napari_cellspots/__init__.py
@@ -1,4 +1,8 @@
-__version__ = "0.1.0"
+from ._version import version as __version__
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 
 from napari_cellspots._widget1 import CellspotsProcessingWidget
 from napari_cellspots._widget2 import CellspotsPolarWidget
